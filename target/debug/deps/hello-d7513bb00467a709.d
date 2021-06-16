2a608c3b7f8c477729ba73f3306665d3c2cbdb37
James <r.j.shimizu@jaist.ac.jp>
2021-06-16 22:35:59 +0900

    first commit

---
+++ target/debug/deps/hello-d7513bb00467a709.d
@@ -0,0 +1,5 @@
+/home/james/learnCS/rust/hello/target/debug/deps/hello-d7513bb00467a709: src/main.rs
+
+/home/james/learnCS/rust/hello/target/debug/deps/hello-d7513bb00467a709.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/james/learnCS/rust/hello/target/debug/hello: /home/james/learnCS/rust/hello/src/main.rs
