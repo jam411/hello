fd69c4ae169c980e110f2334bc404abfbd4c3e7d
ryoji james <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-22 12:42:50 +0900

    Fixing errors

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ impl ThreadPool {
 
         let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
